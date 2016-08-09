96cb5d158dcda927db41a56b020f456ec19fb630
How 2 see <maxinlin@vip.qq.com>
2016-08-09 09:29:09 +0800

    save

--- math/test.py
+++ math/test.py
@@ -1,4 +1,10 @@
+# -*- coding:utf-8 -*-
 import json
+import pandas as pd
+import numpy as np
+from pandas import DataFrame,Series
+from pylab import *
+import matplotlib
 
 path = 'data/gov.txt'
 
@@ -6,4 +12,18 @@ result = [json.loads(line) for line in open(path)]
 
 time_zone = [rec['tz'] for rec in result if 'tz' in rec]
 
+#为result数据生成一个类似表格的结构
+frame = DataFrame(result)
+# print frame
 
+#填补空白的字段为Unkonwn
+clean_tz = frame['tz'].fillna('Missing')
+clean_tz[clean_tz == ''] = 'Unknown'
+
+#通过value_counts方法数得tz下的各项value的个数
+tz_counts = clean_tz.value_counts()
+# print tz_counts
+
+
+tz_counts[:10].plot(kind='barh', rot=0)
+show()
