3c0a2d24a7d3cce0244f989da54636450ee0cef1
How 2 see <maxinlin@vip.qq.com>
2016-08-26 10:14:37 +0800

    save

--- Flask blog/config.py
+++ Flask blog/config.py
@@ -2,7 +2,7 @@
 import os
 import MySQLdb
 
-db = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='blog')
+db = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='blog', charset='utf8')
 
 
 #用户状态
--- Flask blog/test.py
+++ Flask blog/test.py
@@ -1,40 +1,8 @@
 #coding:utf-8
+from config import db
 
-import urllib
-import urllib2
-import json
-print '===============开始测试==========================='
-test_data = ['59596193526', '59597922100', '59620525284']
-headers = {'Content-Type': 'application/json'}
-requrl = "http://3dtest.1jbest.com/yijiaifuwo/item/modeltype/"
-req = urllib2.Request(url = requrl,data =json.dumps(test_data), headers=headers)
-print req
-res_data = urllib2.urlopen(req)
-res = res_data.read()
-print res
-
-print '=====================模型菜单测试完成=============='
-
-test_data = ['YFNE-43-99-01', 'YFNE-43-88-01', 'YFNE-43-89-01']
-headers = {'Content-Type': 'application/json'}
-requrl = "http://3dtest.1jbest.com/yijiaifuwo/item/preview/"
-req = urllib2.Request(url = requrl,data =json.dumps(test_data), headers=headers)
-print req
-res_data = urllib2.urlopen(req)
-res = res_data.read()
-print res
-
-print '=====================获取预览图测试完成=============='
-
-test_data = {'designerId':2,'salable':1,'product_name':'test','product_link':'http://www.test.com',
-             'product_price':'90.2','unit':1,'discount_price':'20','is_public':1,'trim_width':11,'trim_length':11,'trim_height':11,
-             'skucode':'123ss123'}
-headers = {'Content-Type': 'application/json'}
-requrl = "http://3dtest.1jbest.com/yijiaifuwo/item/update/"
-req = urllib2.Request(url = requrl,data =json.dumps(test_data), headers=headers)
-print req
-res_data = urllib2.urlopen(req)
-res = res_data.read()
-print res
-
-print '=====================新建物品测试完成==============='
+cursor = db.cursor()
+sql = 'select username from account limit 10'
+cursor.execute(sql)
+a = cursor.fetchall()
+print a
