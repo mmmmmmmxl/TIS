c63870234e22ac36b2f02b70614384a02a36d62d
How 2 see <maxinlin@vip.qq.com>
2016-09-05 14:33:43 +0800

    save

--- Flask blog/test.py
+++ Flask blog/test.py
@@ -1,14 +1,4 @@
 #coding:utf-8
 
-import os
-
-path = 'D:\Documents\GitHub\study\Flask blog\\blog\static\img\page'
-print path
-list = os.listdir(path)
-a = 1
-for i in list:
-    old = os.path.join(path,i)
-    new_name = str(a)+'.jpg'
-    new = os.path.join(path,new_name)
-    os.rename(old,new)
-    a += 1
+data = {'no':'sdwq123sd',
+        'user_id':'1223sds2'}
