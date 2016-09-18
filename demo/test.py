41f9cb793f2b5c245580e5f76407411ef5ea2a9b
How 2 see <maxinlin@vip.qq.com>
2016-09-18 16:37:26 +0800

    save

--- demo/test.py
+++
@@ -1,6 +0,0 @@
-#coding:utf-8
-
-import json
-
-a = {'houseTypeId': u'3d4d0471942c4eb9a4dda6f4ed598ca1', 'status': 1, 'fuwoHouseTypeId': u'5a4f3190765911e6bbd000163e003d10', 'createdBy': u'605ef92ba6df4abaa9f79ef928e824c8', 'houseTypeImageUrl': u'http://1jbest-3dtest-img.img-cn-hangzhou.aliyuncs.com/ifuwo/1609/09/6bb0809c765911e692dd00163e003d10.jpg'}
-print json.dumps(a)
