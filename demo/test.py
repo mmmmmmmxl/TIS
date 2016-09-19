826f4fe1844d1f3f468820cbdce57f98ce56977b
How 2 see <maxinlin@vip.qq.com>
2016-09-19 17:33:47 +0800

    save

--- demo/test.py
+++ demo/test.py
@@ -1,17 +1,12 @@
 #encoding: utf-8
 #!/usr/bin/python
 
-import socket
+from itertools import izip
 
-HOST='127.0.0.1'
-PORT=8000
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.connect((HOST,PORT))
-while 1:
-       cmd=raw_input("坐标")
-       s.sendall(cmd)
-       data=s.recv(1024)
-       from time import sleep
-       sleep(0.2)
+a = [1,2,3,4,5,6,7,8,9,0]
+b = ['a','b','c','d','e','f','g','h','i','j']
 
-s.close()
+
+print zip(a,b)
+print dict(izip(a,b))
+
